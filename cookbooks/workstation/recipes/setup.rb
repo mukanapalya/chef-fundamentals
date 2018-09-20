8aa39e0be7c374dcb59ef5dfc847088cb8d06c65
Param <mukanapalya@gmail.com>
2018-09-20 17:48:47 +0000

    implemented template for workstation cookbook

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -20,14 +20,7 @@ node['memory']['total']
 apple_count = 4
 puts "I have #{apple_count} apples"
 
-file '/etc/motd' do
-  content "This server is the property of DRYiCE
-  hostname: #{node['hostname']}
-  ipadress: #{node['ipaddress']}
-  cpu: #{node['cpu']['0']['mhz']}
-  memory: #{node['memory']['total']}
-"
+template '/etc/motd' do
+  source 'motd.erb'
   action :create
-  owner 'root'
-  group 'root'
 end
