fc7d30dc771c30a4bc6b08d4ec336ddc671eed99
mukanapalya <43425150+mukanapalya@users.noreply.github.com>
2018-09-21 10:44:31 -0400

    remote commit: a minor change to default.rb file

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,4 +4,5 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 # Adding a line for rebase example checking, more chnage from extra branch too
+# Adding remotely
 include_recipe 'apache::server'
