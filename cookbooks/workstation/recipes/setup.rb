d8ba4789f52b1b6f6aaff78394d4e5daa1a8fdba
Param <mukanapalya@gmail.com>
2018-09-21 16:06:46 +0000

    user funcationality after stash apply

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -1,4 +1,4 @@
-# setup file for utilities
+# setup file for utilities before stashing
 package 'git'
 package 'vim-enhanced'
 
@@ -19,6 +19,17 @@ node['memory']['total']
 
 apple_count = 4
 puts "I have #{apple_count} apples"
+
+user 'user1' do
+  comment 'user1'
+  uid '123'
+  home '/home/user1'
+  shell '/bin/bash'
+end
+
+group 'admins' do
+  members 'user1'
+end
 
 template '/etc/motd' do
   source 'motd.erb'
